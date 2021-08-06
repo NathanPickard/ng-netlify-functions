ba74411c371c315b8d6094feb990e2fd23afcff7
Nathan Pickard <nathan.pickard87@gmail.com>
2021-08-05 21:26:32 -0700

    Product list and buy now button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import products from './products.json';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ng-netlify-functions';
+  products: any = products;
 }
