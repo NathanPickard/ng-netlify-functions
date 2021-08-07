8a0e964452b7e6fa550382c28164e9bcfbde4ef0
Nathan Pickard <nathan.pickard87@gmail.com>
2021-08-07 11:21:58 -0700

    triggerCreateCheckout and openStripe functions added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+
 import products from './products.json';
+
+import { loadStripe } from '@stripe/stripe-js';
 
 @Component({
   selector: 'app-root',
@@ -15,7 +18,21 @@ export class AppComponent {
   constructor(private http: HttpClient) {}
 
   async triggerCreateCheckout(eventProduct: any) {
-    
+    this.response = await this.http
+      .post('./netlify/functions/createCheckout', eventProduct, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      .toPromise();
+    this.openStripe(this.response);
   }
+
+  openStripe = async (stripeParams: any) => {
+    const stripe = await loadStripe(stripeParams.publishableKey);
+    const { error } = await stripe!.redirectToCheckout({
+      sessionId: stripeParams.sessionId
+    });
+  };
 
 }
